74223b59f985188198ea71e8603eb0d2679ca26e
joaoguilherme1 <joaoguilhermesouza1@gmail.com>
2021-10-02 01:07:34 -0300

    Heif to jpeg view done

--- tudoparapdf/main/views.py
+++ tudoparapdf/main/views.py
@@ -1,14 +1,14 @@
+from os import name
 from django.conf import settings
 from django.shortcuts import render
 from django.urls.conf import path
 from django.views.generic import TemplateView
 from django.http import HttpResponse, FileResponse, response
-import random
+import random as rd
 import pyheif
 import tempfile
 from PIL import Image
-import os
-import pathlib
+import zipfile as zp
 
 # def download(request):
 #     file_server = pathlib.Path('/static/')
@@ -21,28 +21,37 @@ import pathlib
 #         return response
 #     return redirect('a_url_path')
 
-def index_test(request):
+def heif_to_jpeg(request):
 
     if request.method == 'GET':
         return render(request, 'home.html')
 
     elif request.method == 'POST':
-        tmp = tempfile.NamedTemporaryFile(delete=False)
-        nome = request.POST['nome_arquivo']
-        heif_file = pyheif.read(request.FILES['myfile'].temporary_file_path())
-        image = Image.frombytes(
-            heif_file.mode, 
-            heif_file.size, 
-            heif_file.data,
-            "raw",
-            heif_file.mode,
-            heif_file.stride,
-            )
-        image.save("/tmp/"+nome, "JPEG")
-        #tmp = tempfile.NamedTemporaryFile(delete=False)
-            # write to your tempfile, mode may vary
-        #fl = open(request.FILES['myfile'].temporary_file_path(), 'rb')
-        fl = open("/tmp/"+nome, 'rb')
-        response = FileResponse(fl)
-        response['Content-Disposition'] = "attachment; filename=%s" % nome
-        return response
+
+        name_files = request.POST['nome_arquivo']
+        name_dir = request.POST['nome_pasta']
+        name_dir_with_main_dir = f'/tmp/{name_dir}.zip'
+        images_heif = request.FILES.getlist('file[]')
+
+        if name_files == '':
+            name_files = 'Convertendo'
+        if name_dir == '':
+            name_dir = 'Zipando'
+        middle_zipfile = zp.ZipFile(name_dir_with_main_dir, 'w')
+
+        for i in range(len(images_heif)):
+            heif_file = pyheif.read(images_heif[i].temporary_file_path())
+            heif_file_to_jpeg = Image.frombytes(
+                heif_file.mode, 
+                heif_file.size, 
+                heif_file.data,
+                "raw",
+                heif_file.mode,
+                heif_file.stride,
+                )
+            heif_file_to_jpeg.save(f'/tmp/{name_files}_{i}.jpeg', "JPEG")
+            middle_zipfile.write(f'/tmp/{name_files}_{i}.jpeg')
+
+        middle_zipfile.close()
+        final_zipfile = open(name_dir_with_main_dir, 'rb')
+        return FileResponse(final_zipfile)
