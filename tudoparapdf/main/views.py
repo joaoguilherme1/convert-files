4e2b596886e1407bac4428dbf97d281aefd03a3e
joaoguilherme1 <joaoguilhermesouza1@gmail.com>
2021-10-05 21:31:53 -0300

    Introduzindo limitadores de arquivos e tamanho de arquivos

--- tudoparapdf/main/views.py
+++ tudoparapdf/main/views.py
@@ -1,7 +1,8 @@
 from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls.conf import path
 from django.template.response import TemplateResponse
+from django.core.exceptions import EmptyResultSet, MultipleObjectsReturned, PermissionDenied
 #from django.views.generic import TemplateView
 from django.http import FileResponse #, HttpResponse
 import pyheif
@@ -14,38 +15,50 @@ def home(request):
     return TemplateResponse(request, 'home.html', {})
 
 
-def heif_to_jpeg(request):
+def heic_to_jpeg(request):
 
     if request.method == 'GET':
 
-        return render(request, 'heif_to_jpeg.html')
+        return render(request, 'heic_to_jpeg.html')
 
     elif request.method == 'POST':
 
+        horario_do_request = tm.strftime("(%d/%m/%Y)|%H:%M", tm.localtime()) # BUGANDO
         name_files = request.POST['nome_arquivo']
         name_dir = request.POST['nome_pasta']
         name_dir_with_main_dir = f'/tmp/{name_dir}.zip'
-        images_heif = request.FILES.getlist('file[]')
+        images_heic = request.FILES.getlist('file[]')
 
         if name_files == '':
             name_files = 'Convertendo'
-        if name_dir == '':
+        elif name_dir == '':
             name_dir = 'Zipando'
-        middle_zipfile = zp.ZipFile(name_dir_with_main_dir, 'w')
 
-        for i in range(len(images_heif)):
-            heif_file = pyheif.read(images_heif[i].temporary_file_path())
-            heif_file_to_jpeg = Image.frombytes(
-                heif_file.mode, 
-                heif_file.size, 
-                heif_file.data,
-                "raw",
-                heif_file.mode,
-                heif_file.stride,
-                )
-            heif_file_to_jpeg.save(f'/tmp/{name_files}_{i}.jpeg', "JPEG")
-            middle_zipfile.write(f'/tmp/{name_files}_{i}.jpeg')
+        if len(images_heic) == 0 or len(images_heic) >= 4:
+            raise PermissionDenied('É necessario selecionar no minimo 1 e no maximo 4 para converter')
 
-        middle_zipfile.close()
-        final_zipfile = open(name_dir_with_main_dir, 'rb')
-        return FileResponse(final_zipfile)
+        else:
+
+            middle_zipfile = zp.ZipFile(name_dir_with_main_dir, 'w')
+
+            for i in range(len(images_heic)):
+
+                if images_heic[i].size > 4000000:
+                    raise PermissionDenied('O arquivo deve ser menor que 4mb')
+
+                else:
+                    heif_file = pyheif.read(images_heic[i].temporary_file_path())
+                    heif_file_to_jpeg = Image.frombytes(
+                        heif_file.mode, 
+                        heif_file.size, 
+                        heif_file.data,
+                        "raw",
+                        heif_file.mode,
+                        heif_file.stride,
+                        )
+                    heif_file_to_jpeg.save(f'/tmp/{name_files}_{i}.jpeg', "JPEG")
+                    middle_zipfile.write(f'/tmp/{name_files}_{i}.jpeg')
+
+            middle_zipfile.close()
+            final_zipfile = open(name_dir_with_main_dir, 'rb')
+            return FileResponse(final_zipfile)
